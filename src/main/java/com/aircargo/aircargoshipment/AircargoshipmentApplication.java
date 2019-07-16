5286483ad882d76b0199b7d62fc76c3214d4ef53
K <563379@cognizant.com>
2019-07-16 12:27:20 +0530

    API SpringBoot

---
+++ src/main/java/com/aircargo/aircargoshipment/AircargoshipmentApplication.java
@@ -0,0 +1,13 @@
+package com.aircargo.aircargoshipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircargoshipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircargoshipmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aircargo/aircargoshipment/AircargoshipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aircargo.aircargoshipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircargoshipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
